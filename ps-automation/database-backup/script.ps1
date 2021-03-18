e82bf5e701d169074cd467ad593b2105c69cf904
Oluwatosin Ayanda <oluwatosin.ayanda@ledd.com>
2021-03-18 09:57:42 +0300

    Added Support For Backing Up DB Using Its Connection String

--- ps-automation/database-backup/script.ps1
+++ ps-automation/database-backup/script.ps1
@@ -9,22 +9,27 @@ https://www.varonis.com/blog/windows-powershell-tutorials/
 https://adamtheautomator.com/powershell-check-if-file-exists/
 ####>
 
+
 ####
 ## Declare Arguments
 ####
 param ([string]$connectionString,
-       [string]$serverName= $CPU,
+       [string]$serverName,
        [string]$databaseName,
-       [string]$backupLocation=$DEFAULT_BACKUP_LOCATION,
-       [string]$backupFileName=$DEFAULT_BACKUP_FILENAME)
+       [string]$backupLocation,
+       [string]$backupFileName,
+       [switch]$UseConnectionString = $false)
 
 ####
 ## Declare Global Variables
 ####
 $CPU = $env:COMPUTERNAME
-$DEPENDENCIES = @('SqlServer','Write-Log')
-$DEFAULT_BACKUP_LOCATION = 'C:\db\backup'
-$DEFAULT_BACKUP_FILENAME = "backup.bak"
+$CURRENT_USER = $env:UserName
+$DEPENDENCIES = @('SqlServer','Write-Log');
+$DEFAULT_BACKUP_LOCATION = 'C:\db\backup';
+$DEFAULT_BACKUP_FILENAME = "backup.bak";
+
+
 
 ####
 ## Declare Functions
@@ -97,6 +102,7 @@ function Install-Dependencies
                 Write-Host "Installing Module"
                 # Install it...
                 Install-Module -Name $module -Scope CurrentUser -Force
+                
                 Write-Host "Successfully Installed Module"
             }
         }
@@ -113,7 +119,7 @@ function Install-Dependencies
 
 }
 
-function Verify-BackupLocationExists {
+function Verify-LocationExists {
 
     Param ([string]$path,
            [bool]$createDirectory = $false)
@@ -169,6 +175,7 @@ function Test-MrCmdletBinding {
 write-host -ForegroundColor Green "************************************************************"
 write-host -ForegroundColor White "Starting PowerShell Script Execution"
 write-host -ForegroundColor Green "Title : Backup Database"
+write-host -ForegroundColor Green "Hostname : $CPU"
 write-host -ForegroundColor Green "************************************************************"
 
 # Install Module Dependencies
@@ -185,41 +192,115 @@ Verify-BackupLocationExists -path "C:\logs"
 !(Verify-BackupLocationExists -path "C:\lbus")
 #>
 
-#Provide Computer Name as Default Value For Server, If Empty
-#if(($serverName -eq '.') -or ($serverName -eq ''))
-if(([string]::IsNullOrEmpty($serverName)) -or ($serverName -eq '.'))
+if($UseConnectionString)
 {
-    $serverName = $CPU;
-}
 
-if((Verify-ServerExistsAndReachable -a $serverName) -ne $true)
-{
-    write-host -ForegroundColor Red "Server $serverName is not Reachable"    
+    #Loading the SMO assembly into your PowerShell session. You can actually do this by simply loading the full SQLPS module, as that will automatically load the assembly
+    #Add-Type -AssemblyName "Microsoft.SqlServer.Smo,Version=11.0.0.0,Culture=neutral,PublicKeyToken=89845dcd8080cc91"
+    #Install-Module -Name Microsoft.SqlServer.Smo -Scope CurrentUser -Force
+    #System.Reflection.Assembly::LoadWithPartialName("Microsoft.SqlServer.Smo")
+    [System.Reflection.Assembly]::LoadWithPartialName("Microsoft.SqlServer.Smo")
+
+    try
+    {
+        # Create Connection
+        Write-Host -ForegroundColor Yellow "Creating Connection Object ...."
+        $sqlConn = New-Object System.Data.SqlClient.SqlConnection
+        $sqlConn.ConnectionString = “$connectionString”
+        $sqlConn.Open()
+        Write-Host -ForegroundColor Green "Connection Established"
+
+        # Create Backup
+        write-host -ForegroundColor Yello "Starting Backup"
+        #$databaseName = $connectionString.Split()
+        $databaseName = "pob-test"
+
+        $sqlcmd = $sqlConn.CreateCommand()
+        <# or 
+        
+         $sqlcmd = New-Object System.Data.SqlClient.SqlCommand
+         
+         #>
+        # Create Directory If It Does Not Exist
+        if((Verify-LocationExists -path "$DEFAULT_BACKUP_LOCATION\$CURRENT_USER") -ne $true)
+        {
+            New-Item -Path "$DEFAULT_BACKUP_LOCATION\$CURRENT_USER" -ItemType Directory        
+        }
+
+        $backupPath = "$DEFAULT_BACKUP_LOCATION\$CURRENT_USER\$DEFAULT_BACKUP_FILENAME"
+
+        $sqlcmd.Connection = $sqlConn
+        $query = “BACKUP DATABASE [@dbname] TO DISK=@diskpath”
+        Write-Host $query         #Use Parameterised Query in the Command        $sqlcmd.CommandText = $query
+        $sqlcmd.Parameters.Add("@dbname", $databaseName);
+        $sqlcmd.Parameters.Add("@diskpath", $backupPath);
+
+        $sqlcmd.CommandTimeout = 0
+
+
+        $sqlcmd.ExecuteNonQuery() | Out-Null
+
+        write-host -ForegroundColor Yello "Backup Completed"
+
+
+    }
+    catch
+    {
+        Write-Host -ForegroundColor Red "Unable to Complete DataBase Backup"
+        Write-Output $PSItem.ToString()
+    }
+    finally
+    {
+        if($sqlConn -ne $null)
+        {
+            Write-Host -ForegroundColor Yellow "Closing Connection ...."
+            $sqlConn.Close()
+            Write-Host -ForegroundColor Green "Connection Terminated"
+
+        }
+    }
+    
+
 }
-elseif((Verify-BackupLocationExists -path $backupPath) -ne $true)
-{
-    write-host -ForegroundColor Red "Backup $backupPath Path Does Not exist"    
-}
-# Perform Backup
 else
 {
-    write-host -ForegroundColor Yello "Starting Backup"
-    #Backup-SqlDatabase -ServerInstance "." -Database "testdb" -BackupFile "c:\sql\testDB.bak"
-    $extension = ".bak"
-    $backupPath=''
-    $backupPath = "$backupLocation$backupFileName$extension"
-
-     # Check If File With Matching Name Found
-    if(Test-Path -Path $backupPath -PathType Leaf)
+    #Provide Computer Name as Default Value For Server, If Empty
+    #if(($serverName -eq '.') -or ($serverName -eq ''))
+    if(([string]::IsNullOrEmpty($serverName)) -or ($serverName -eq $null))
     {
-        # Rename Old Backup File
-        $currentTimeStamp = Get-Date -Format "MM-dd-yyyy HH:mm:ss"
-        $renamedPath = $backupPath.Replace($extension,$currentTimeStamp +$extension)
-        Rename-Item -Path $backupPath -NewName $renamedPath
+        $serverName = $CPU;
     }
-    write-host -ForegroundColor White "Backup Path is $backupPath"
-    Backup-SqlDatabase -ServerInstance $serverName -Database $databaseName -BackupFile $backupPath
+
+    if((Verify-ServerExistsAndReachable -a $serverName) -ne $true)
+    {
+        write-host -ForegroundColor Red "Server $serverName is not Reachable"    
+    }
+    elseif((Verify-LocationExists -path $backupPath) -ne $true)
+    {
+        write-host -ForegroundColor Red "Backup $backupPath Path Does Not exist"    
+    }
+    # Perform Backup
+    else
+    {
+        write-host -ForegroundColor Yello "Starting Backup"
+        #Backup-SqlDatabase -ServerInstance "." -Database "testdb" -BackupFile "c:\sql\testDB.bak"
+        $extension = ".bak"
+        $backupPath=''
+        $backupPath = "$backupLocation$backupFileName$extension"
+
+         # Check If File With Matching Name Found
+        if(Test-Path -Path $backupPath -PathType Leaf)
+        {
+            # Rename Old Backup File
+            $currentTimeStamp = Get-Date -Format "MM-dd-yyyy HH:mm:ss"
+            $renamedPath = $backupPath.Replace($extension,$currentTimeStamp +$extension)
+            Rename-Item -Path $backupPath -NewName $renamedPath
+        }
+        write-host -ForegroundColor White "Backup Path is $backupPath"
+        Backup-SqlDatabase -ServerInstance $serverName -Database $databaseName -BackupFile $backupPath
+    }
 }
+
 
 
 write-host -ForegroundColor Green "************************************************************"
